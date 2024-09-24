f781daacfca8630042a3a1d35cdccc47881afce3
farid.ansari <farid.ansari@gmail.com>
2024-09-23 21:55:54 -0700

    LLM Personality page PUT functionality added.

--- client/src/components/PersonalityConfig/PersonalityConfig.jsx
+++ client/src/components/PersonalityConfig/PersonalityConfig.jsx
@@ -1,25 +1,44 @@
 import "./PersonalityConfig.scss";
+import { useRef, useState } from "react";
 
-export default function PersonalityConfig({ personalityObj }) {
+export default function PersonalityConfig({ personalityObj, updateCallBack }) {
+  const [formData, setFormData] = useState({ ...personalityObj });
+
+  const userInputName = useRef();
+  const userInputTemperature = useRef();
+  const userInputPrompt = useRef();
+
+  const handleSave = (e) => {
+    e.preventDefault();
+     updateCallBack({ "personalityID": personalityObj.personalityID, "name": userInputName.current.value,  "avatarImg": personalityObj.avatarImg, "temperature": userInputTemperature.current.value, "conditionPrompt": userInputPrompt.current.value });
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  }
+
   return (
     <>
       <div className="PersonalityConfig">
-        <div className="PersonalityConfig__group">
-          <p className="PersonalityConfig__group__label">Name:</p>
-          < input className="PersonalityConfig__group__data" type="text" value={personalityObj.name}></input>
-        </div>
-        <div className="PersonalityConfig__group">
-          <p className="PersonalityConfig__group__label">Temperature:</p>
-          < input className="PersonalityConfig__group__data" type="text" value={personalityObj.temperature}></input>
-        </div>
-        <div className="PersonalityConfig__group">
-          <p className="PersonalityConfig__group__label">Prompt:</p>
-          < input className="PersonalityConfig__group__data" type="text" value={personalityObj.conditionPrompt}></input>
-        </div>        
-        <div className="PersonalityConfig__group">
-          <button>Save</button>
-          <div></div>
-        </div>  
+        <form id={personalityObj.personalityID} onSubmit={handleSave}>
+          <div className="PersonalityConfig__group">
+            <p className="PersonalityConfig__group__label">Name:</p>
+            <input ref={userInputName} name="name" className="PersonalityConfig__group__data" type="text" value={formData.name} onChange={handleChange}></input>
+          </div>
+          <div className="PersonalityConfig__group">
+            <p className="PersonalityConfig__group__label">Temperature:</p>
+            <input ref={userInputTemperature} name="temperature" className="PersonalityConfig__group__data" type="text"  value={formData.temperature} onChange={handleChange}></input>
+          </div>
+          <div className="PersonalityConfig__group">
+            <p className="PersonalityConfig__group__label">Prompt:</p>
+            <input ref={userInputPrompt} name="conditionPrompt" className="PersonalityConfig__group__data" type="text" value={formData.conditionPrompt} onChange={handleChange}></input>
+          </div>
+          <div className="PersonalityConfig__group">
+            <button type="submit">Save</button>
+            <div></div>
+          </div>
+        </form>
       </div>
     </>
   );
--- client/src/pages/PersonalitiesPage/PersonalitiesPage.jsx
+++ client/src/pages/PersonalitiesPage/PersonalitiesPage.jsx
@@ -1,14 +1,13 @@
 import "./PersonalitiesPage.scss";
-import Sidebar from "../../components/Sidebar/Sidebar"
+import Sidebar from "../../components/Sidebar/Sidebar";
 import PersonalityConfig from "../../components/PersonalityConfig/PersonalityConfig";
 
 import axios from "axios";
 import webapi from "../../utils/webapi";
 import { useState, useRef, useEffect } from "react";
 
-
 export default function PersonalitiesPage() {
-  let [personalities, setPersonalities] = useState([{name: "", avatarImg :"", temperature:0, conditionPrompt:"", personalityID:""}]);
+  let [personalities, setPersonalities] = useState([{ name: "", avatarImg: "", temperature: 0, conditionPrompt: "", personalityID: "" }]);
 
   useEffect(() => {
     const fetchPersonalities = async () => {
@@ -21,17 +20,29 @@ export default function PersonalitiesPage() {
     };
     fetchPersonalities();
   }, []);
-  
+
+  const handleUpdatePersonality = async (personalityObj) => {
+    try {
+      const updateURL = webapi.URL + "/personality/" + personalityObj.personalityID;
+      console.log(updateURL);
+      const response = await axios.put(updateURL, {personalityObj});
+      console.log(response.data);
+    } catch (error) {
+      alert(`PersonalitiesPage.handleUpdatePersonality() request failed with error: ${error}`);
+      return -1;
+    }
+  };
+
   return (
     <div className="PersonalitiesPage">
       <div className="PersonalitiesPage__left">
-        <Sidebar chatSessions ={[]} />
+        <Sidebar chatSessions={[]} />
       </div>
       <div className="PersonalitiesPage__main">
         <div className="PersonalitiesPage__main__content">
-        {personalities.map((i) => (
-            <PersonalityConfig personalityObj={i}/>
-        ))}
+          {personalities.map((i) => (
+            <PersonalityConfig personalityObj={i} updateCallBack={handleUpdatePersonality} />
+          ))}
         </div>
       </div>
     </div>
