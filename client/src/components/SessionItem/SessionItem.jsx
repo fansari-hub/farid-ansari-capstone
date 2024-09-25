cc142a6a66e142a03c1d3a61dcd72f34d8582dd2
farid.ansari <farid.ansari@gmail.com>
2024-09-24 22:28:32 -0700

    Moved Session List Item into their own component. Fixed display bug with adding/removing session. Added key for session list.

---
+++ client/src/components/SessionItem/SessionItem.jsx
@@ -0,0 +1,65 @@
+import "./SessionItem.scss";
+import { useRef } from "react";
+
+export default function SessionItem({ chatSession, switchSessionCallBack, deleteSessionCallback, updateSessionCallback }) {
+  const sessionRef = useRef();
+
+  function handleEditButton() {
+    sessionRef.current.disabled = false;
+    sessionRef.current.focus();
+  }
+
+  function handleUpdateName() {
+    sessionRef.current.disabled = true;
+    updateSessionCallback(chatSession.sessionID, sessionRef.current.value);
+  }
+
+  return (
+    <>
+      <div className="SessionItem__group">
+        <div
+          className="SessionItem__group__itemContainer"
+          onClick={() => {
+            switchSessionCallBack(chatSession.sessionID);
+          }}
+        >
+          <input
+            ref={sessionRef}
+            name="sessionField"
+            type="text"
+            className="SessionItem__group__item"
+            onBlur={() => {
+              handleUpdateName();
+            }}
+            defaultValue={chatSession.sessionName}
+            disabled
+          />
+        </div>
+        {deleteSessionCallback ? (
+          <button
+            className="SessionItem__group__delete"
+            onClick={() => {
+              deleteSessionCallback(chatSession.sessionID);
+            }}
+          >
+            D
+          </button>
+        ) : (
+          <></>
+        )}
+        {updateSessionCallback ? (
+          <button
+            className="SessionItem__group__edit"
+            onClick={(e) => {
+              handleEditButton();
+            }}
+          >
+            E
+          </button>
+        ) : (
+          <></>
+        )}
+      </div>
+    </>
+  );
+}
--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,27 +1,10 @@
 import "./Sidebar.scss";
 import { Link } from "react-router-dom";
 import { useRef, useState } from "react";
+import SessionItem from "../SessionItem/SessionItem";
 
 export default function Sidebar({ chatSessions, switchSessionCallBack, addSessionCallback, deleteSessionCallback, updateSessionCallback }) {
   const fieldListRef = useRef();
-  const [formData, setFormData] = useState(null);
-
-  function handleEditButton(button_name) {
-    const targetField = fieldListRef.current.querySelector(`input[name='${button_name}']`);
-    targetField.disabled = false;
-    targetField.focus();
-  }
-
-  function handleUpdateName(button_name, sessionID) {
-    const targetField = fieldListRef.current.querySelector(`input[name='${button_name}']`);
-    targetField.disabled = true;
-    updateSessionCallback(sessionID, targetField.value);
-  }
-
-  const handleChange = (e) => {
-    // const { name, value } = e.target;
-    // setFormData([...formData, {"name": name, "value": value}]);
-  }
 
   return (
     <>
@@ -37,55 +20,18 @@ export default function Sidebar({ chatSessions, switchSessionCallBack, addSessio
           <Link to="/">
             <p className="Sidebar__sessionList__title">Chat Sessions</p>
           </Link>
-          <form ref={fieldListRef} className="Sidebar__sessionList__list">
-            {chatSessions.map((i, x) => (
-              <div className="Sidebar__sessionList__list__group">
-                <div
-                  className="Sidebar__sessionList__list__group__itemContainer"
-                  onClick={() => {
-                    switchSessionCallBack(i.sessionID);
-                  }}
-                >
-                  <input
-                    name={`session_${x}`}
-                    type="text"
-                    className="Sidebar__sessionList__list__group__item"
-                    onBlur={() => {
-                      handleUpdateName(`session_${x}`, i.sessionID);
-                    }}
-                    onChange={handleChange} defaultValue={i.sessionName}
-                    disabled
-                  />
-                </div>
-                {deleteSessionCallback ? (
-                  <button
-                    className="Sidebar__sessionList__list__group__delete"
-                    onClick={() => {
-                      deleteSessionCallback(i.sessionID);
-                    }}
-                  >
-                    D
-                  </button>
-                ) : (
-                  <></>
-                )}
-                {updateSessionCallback ? (
-                  <button
-                    className="Sidebar__sessionList__list__group__edit"
-                    onClick={(e) => {
-                      e.preventDefault();
-                      handleEditButton(`session_${x}`);
-                    }}
-                  >
-                    E
-                  </button>
-                ) : (
-                  <></>
-                )}
-              </div>
-            ))}
-          </form>
-          {addSessionCallback ? <button onClick={addSessionCallback}>+</button> : <></>}
+          <div className="Sidebar__sessionList__list">
+            {chatSessions.map((i, x) => {
+              return <SessionItem chatSession={i} switchSessionCallBack={switchSessionCallBack} deleteSessionCallback={deleteSessionCallback} updateSessionCallback={updateSessionCallback} key={x} />;
+            })}
+          </div>
+          {addSessionCallback ? (
+            <button className="Sidebar__edit" onClick={addSessionCallback}>
+              +
+            </button>
+          ) : (
+            <></>
+          )}
         </div>
       </div>
     </>
