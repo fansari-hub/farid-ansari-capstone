e1232595c36d48d05fee0ed67b0b1920142ecda6
farid.ansari <farid.ansari@gmail.com>
2024-10-01 17:54:05 -0700

    updated status code error handling to be more useful and standard on all HTTP facing functions (controllers).

--- server/controllers/chatSession-controller.js
+++ server/controllers/chatSession-controller.js
@@ -4,16 +4,25 @@ const chatgptController = require("./chatgpt-controller");
 
 const httpCreateSession = async (req, res) => {
   if (!req.body.sessionName || typeof req.body.sessionName !== "string") {
-    res.status(500).json({ "Error Message": "Must provide a session name" });
-    return -1;
+    res.status(401).json({ "Error Message": "Must provide a session name" });
+    return false;
   }
   const result = chatSessionModel.createChatSession(req.body.sessionName)
-  res.status(200).json(result);
+  if (result === false){
+    res.status(500);
+    return false;
+  }
+  res.status(201).json(result);
 };
 
 const httpGetSessions = async (req, res) => {
 const result = chatSessionModel.getChatSessions();
+
 result.then((result) => {
+  if (result === false){
+    res.status(500);
+    return false;
+  }
   res.status(200).json(result);
 })
 };
@@ -21,19 +30,23 @@ result.then((result) => {
 const httpGetSessionHistory = async (req, res) => {
   const result = chatSessionModel.getChatSessionChatDetail(req.params.id);
   result.then((result) => {
+    if (result === false){
+      res.status(500);
+      return false;
+    }
     res.status(200).json(result);
   })
 };
 
 const httpInsertChat = async (req, res) => {
   if (!req.body.senderID || typeof req.body.senderID !== "string") {
-    res.status(500).json({ error: "Must provide a Sender ID String" });
-    return -1;
+    res.status(401).json({ error: "Must provide a Sender ID String" });
+    return false;
   }
 
   if (!req.body.message || typeof req.body.message !== "string") {
-    res.status(500).json({ error: "Must provide a message String" });
-    return -1;
+    res.status(401).json({ error: "Must provide a message String" });
+    return false;
   }
   chatSessionModel.setChatGlobal(req.params.id, req.body.senderID, req.body.message);
   // The HTTP response will be send by chatGPT controller. 
@@ -47,30 +60,50 @@ const httpForceBotChat = async (req, res) => {
 
 const httpDeleteSession = async (req, res) => {
   const result = chatSessionModel.deleteSession(req.params.id);
+  if (result === false){
+    res.status(500);
+    return false;
+  }
   res.status(200).json(result);
 }
 
 const httpUpdateSession = async (req, res) => {
   if (!req.body.sessionName || typeof req.body.sessionName !== "string") {
-    res.status(500).json({ error: "Must provide a Session Name String" });
-    return -1;
+    res.status(401).json({ error: "Must provide a Session Name String" });
+    return false;
   }
   const result = chatSessionModel.updateSession(req.params.id, req.body.sessionName);
+  if (result === false){
+    res.status(500);
+    return false;
+  }
   res.status(200).json(result);
 }
 
 const httpInsertPersonIntoSession = async (req, res) => {
   const result = await chatSessionModel.insertPerson(req.params.id, req.params.personid);
+  if (result === false){
+    res.status(500);
+    return false;
+  }
   res.status(200).json(result);
 }
 
 const httpRemovePersonFromSession = async (req, res) => {
   const result = await chatSessionModel.removePerson(req.params.id, req.params.personid);
+  if (result === false){
+    res.status(500);
+    return false;
+  }
   res.status(200).json(result);
 }
 
 const httpGetPersonsInSession = async (req, res) => {
   const result = await chatSessionModel.getPersons(req.params.id);
+  if (result === false){
+    res.status(500);
+    return false;
+  }
   res.status(200).json(result);
 }
 
