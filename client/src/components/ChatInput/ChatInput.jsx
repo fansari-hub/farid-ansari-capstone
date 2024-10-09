e27dce7ca5b4545e1282f9d7edab034fbbec4c6c
farid.ansari <farid.ansari@gmail.com>
2024-10-08 23:29:49 -0700

    fixed aligning issues

--- client/src/components/ChatInput/ChatInput.jsx
+++ client/src/components/ChatInput/ChatInput.jsx
@@ -4,23 +4,25 @@ import Icon from "../Icon/Icon";
 import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
 
 export default function ChatInput({ sendChatCallBack, userInput, inputTTSflag, inputAutoChatFlag, skipCallBack }) {
-
   return (
     <>
       <div className="ChatInput">
         <img src={logo} className="ChatInput__logo" alt="logo" />
         <div className="ChatInput__input">
-          <textarea className="ChatInput__input__textbox" ref={userInput} id="userInput" rows="4"  placeholder="Type your message here to Janus GPT here."></textarea>
+          <textarea className="ChatInput__input__textbox" ref={userInput} id="userInput" rows="4" placeholder="Type your message here to Janus GPT here."></textarea>
           <div className="ChatInput__input__buttons">
-            <div className="ChatInput__input__buttons__button" onClick={sendChatCallBack} ><Icon iconIndex={3} iconName={"Send"} actionType="neutral" /></div>
-            <div className="ChatInput__input__buttons__button" onClick={skipCallBack} ><Icon iconIndex={13} iconName={"Pass"} actionType="neutral" /></div>
-            <div className="ChatInput__input__buttons__group">
-            <ToggleSwitch toggleReference={inputTTSflag} defaultState={false} iconName="Speech" hideLabel={false}/>
+            <div className="ChatInput__input__buttons__button" onClick={sendChatCallBack}>
+              <Icon iconIndex={3} iconName={"Send"} actionType="neutral" />
+            </div>
+            <div className="ChatInput__input__buttons__button" onClick={skipCallBack}>
+              <Icon iconIndex={13} iconName={"Pass"} actionType="neutral" />
             </div>
             <div className="ChatInput__input__buttons__group">
-            <ToggleSwitch toggleReference={inputAutoChatFlag} defaultState={false} iconName="Auto" hideLabel={false}/>
+              <ToggleSwitch toggleReference={inputTTSflag} defaultState={false} iconName="TTS" hideLabel={false} />
             </div>
-            
+            <div className="ChatInput__input__buttons__group">
+              <ToggleSwitch toggleReference={inputAutoChatFlag} defaultState={false} iconName="Auto" hideLabel={false} />
+            </div>
           </div>
         </div>
       </div>
