21d454e3974b7aadbea19f515f0ad6a79c3bc5e6
farid.ansari <farid.ansari@gmail.com>
2024-10-17 21:45:05 -0700

    implemented rate limiting and slowdown on backend API endpoints

--- server/routes/personality-routes.js
+++ server/routes/personality-routes.js
@@ -1,7 +1,25 @@
 const router = require("express").Router();
 const pesonalityController = require("../controllers/personality-controller");
 const authorization = require("../middlewares/authorization")
-router.use(authorization.decodeToken);
+const rateLimit = require('express-rate-limit');
+const slowDown = require('express-slow-down');
+
+
+const rateLimiter = rateLimit({
+	windowMs: 0.1667 * 60 * 1000, // 10 second window
+	limit: 10, // Limit each IP to x requests per window
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+})
+
+const speedLimiter = slowDown({
+  windowMs: 1 * 60 * 1000, // 1 minute window
+	delayAfter: 15, // Allow x requests per window
+	delayMs: (hits) => hits * 100, // Add 100 ms of delay to every request after the grace count
+})
+
+
+router.use(rateLimiter, speedLimiter, authorization.decodeToken);
 
 router.route("/")
 .post(pesonalityController.httpCreatePersonality)
